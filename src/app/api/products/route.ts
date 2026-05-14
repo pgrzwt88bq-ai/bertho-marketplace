882c2db5ea7094666932ebe911ac0953a2260898
pgrzwt88bq-ai <pgrzwt88bq@privaterelay.appleid.com>
2026-05-14 23:05:03 +0100

    Update route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,12 +1,27 @@
 export async function GET() {
-  const res = await fetch('https://vendure-king.onrender.com/shop-api', {
+  const res = await fetch(process.env.VENDURE_SHOP_API_URL!, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'vendure-token': 'bertho-token'
+      'vendure-token': process.env.VENDURE_CHANNEL_TOKEN!
     },
     body: JSON.stringify({
-      query: `{ products(options: { take: 10 }) { totalItems items { id name slug variants { priceWithTax { amount currencyCode }`
+      query: `{
+        products(options: { take: 10 }) {
+          totalItems
+          items {
+            id
+            name
+            slug
+            variants {
+              priceWithTax {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
+      }`
     }),
     cache: 'no-store'
   });
