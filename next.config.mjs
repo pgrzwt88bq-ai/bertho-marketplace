0bfbbc9af1ea8d399866f081dcebbf88df8180a7
pgrzwt88bq-ai <pgrzwt88bq@privaterelay.appleid.com>
2026-05-26 12:20:15 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        dangerouslyAllowLocalIP: true,
+        remotePatterns: [
+            {
+                hostname: 'readonlydemo.vendure.io',
+            },
+            {
+                hostname: 'demo.vendure.io'
+            },
+            {
+                hostname: 'localhost'
+            }
+        ],
+    },
+    experimental: {
+        rootParams: true
+    }
+};
+
+export default withNextIntl(nextConfig);
